aedd8b9311aeb14bc87f01b758f3b83ee27f93a9
VardanianA <anivardanian99@gmail.com>
2021-10-26 17:36:29 +0400

    add table task

---
+++ task6/backend/Main.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const table_router = require('./routers/tbl_routers');
+const routers = require('./routers/routers');
+
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log(("Connected to the db")));
+
+
+app.use("", require("./routers/routers"))
+app.use('/accounts', table_router);
+app.use('/accounts/accounts/:id', routers);
+
+
+app.listen(3001, () => {
+    console.log(`Server started at http://localhost:${3001}`);
+})
